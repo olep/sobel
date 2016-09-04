0714bf151e91718c5321a89e1f4f3b5f7b6e92e8
Ole Petter <olepetter.lund@gmail.com>
2016-09-04 23:29:06 +0200

    first commit

---
+++ design/ram.vhd
@@ -0,0 +1,34 @@
+-- filename: ram.vhd
+-- date: 04.09.2016
+
+----------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use sobel_package.all;
+----------------------------------------------------------------------------
+entity ram is
+	port (
+		clk : in std_logic;
+		we : in std_logic; 
+		address : in std_logic_vector(C_ADDRESS_SIZE-1 downto 0);
+		data : inout std_logic_vector(C_WORD_SIZE-1 downto 0)
+	);
+end entity;
+----------------------------------------------------------------------------
+architecture rtl of ram is
+	type memory is array (0 to C_WIDTH_MAX*C_HEIGHT_MAX) of std_logic_vector(C_WORD_SIZE-1 downto 0);
+	signal myram : memory;
+begin
+	process (clk):
+	begin
+		if rising_edge(clk) then 
+			if (we = '1') then
+				myram(address) <= data;
+			else
+				data <= myram(address);
+			end if;
+		end if;
+	end process;
+end architecture;
+----------------------------------------------------------------------------
+
---
+++ design/sobel_controller.vhd
@@ -0,0 +1,23 @@
+-- filename: sobel_controller.vhd
+-- date: 26.08.2016
+
+----------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use sobel_package.all;
+----------------------------------------------------------------------------
+entity sobel_controller is
+	port (
+		clk : in std_logic;
+		rst : in std_logic; 
+		
+		
+	);
+end entity;
+----------------------------------------------------------------------------
+architecture rtl of sobel_controller is
+begin
+
+end architecture;
+----------------------------------------------------------------------------
+
